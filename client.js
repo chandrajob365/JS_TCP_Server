42d1ddf11a4fddcfcf33b983cdd856573890e56d
Manish <chandrajob365@gmail.com>
2017-07-20 09:29:02 +0530

    Add space between functions

--- client.js
+++ client.js
@@ -1,14 +1,17 @@
 const net = require('net')
 const clientSocket = net.connect({port: 9000, host: 'localhost'})
+
 clientSocket.on('connect', () => {
   console.log('Client connected')
 })
+
 /* Sends data on the socket */
 /* Callback parameter will be executed when the data
   is finally written out - this may not be immediately */
 clientSocket.on('data', (msg) => {
   console.log('In client socket ', msg.toString())
 })
+
 setTimeout(() => {
   clientSocket.end(() => {
     console.log('disconnected from server')

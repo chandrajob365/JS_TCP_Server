6093a91662f34d0e474abdfc12a1b9564fe3edc0
Manish <chandrajob365@gmail.com>
2017-07-21 08:51:59 +0530

    Add basic parser for request header and body

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
 const net = require('net')
 
 const server = net.createServer(socket => {
+  let request = {}
+  request['Req-Line'] = {}
+  request['body'] = {}
   console.log('Server connected ')
-  socket.on('data', msg => {
-    console.log('<data event > msg from client @ ', socket.remoteAddress, '  = ', msg.toString())
-    socket.write('You said ' + msg + ' ')
-  })
-  socket.on('close', () => {
-    console.log('CLOSED ' + socket.remoteAddress + ' @ ' + socket.remotePort)
-  })
+  dataEventHandler(socket, request)
+  closeEventHandler(socket)
 }).listen(3000, () => {
   console.log('SERVER listening @ ', JSON.stringify(server.address()))
 })
+
+const closeEventHandler = socket => {
+  socket.on('close', () => {
+    console.log('CLOSED ' + socket.remoteAddress + ' @ ' + socket.remotePort)
+  })
+}
+const dataEventHandler = (socket, request) => {
+  socket.on('data', rawRequest => {
+    requestHandler(request, rawRequest)
+    socket.write('You said ' + rawRequest + ' ')
+  })
+}
+
+const requestHandler = (request, rawRequest) => {
+  parseRawRequest(request, rawRequest.toString())
+}
+
+const parseRawRequest = (request, requestString) => {
+  let requestParts = requestString.split('\r\n\r\n')
+  let headerPart = requestParts[0].replace(/\r/g, '').split('\n')
+  parseRequestLine(request, headerPart[0])
+  parseRequestHeader(request, headerPart.slice(1))
+  console.log('request Headers = ', request)
+  if (request['Content-Length']) {
+    parseRequestBody(request, requestParts[1].replace('\n', ''))
+  }
+}
+
+const parseRequestLine = (request, requestLine, cb) => {
+  requestLine = requestLine.split(' ')
+  console.log('Req-Line = ', requestLine)
+  request['Req-Line'].METHOD = requestLine[0]
+  request['Req-Line'].URI = requestLine[1]
+  request['Req-Line'].VERSION = requestLine[2]
+  console.log('Req-Line = ', request['Req-Line'])
+  if (typeof cb === 'function') cb
+}
+
+const parseRequestHeader = (request, requestHeaders) => {
+  requestHeaders.forEach(header => {
+    header = header.replace(/:/, '&').split('&')
+    request[header[0]] = header[1].replace(/^\s+/g, '')
+  })
+}
+
+const parseRequestBody = (request, msgBody) => {
+  request['body'] = msgBody
+}

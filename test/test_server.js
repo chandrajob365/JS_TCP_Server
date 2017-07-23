9aea8c7f6a6868562624390ba73b05161a976bde
Manish <chandrajob365@gmail.com>
2017-07-24 01:16:11 +0530

    [test_server.js]Add line spacing in function call

--- test/test_server.js
+++ test/test_server.js
@@ -1,8 +1,10 @@
 const Server = require('../lib/server')
 const server = new Server()
+
 server.startServer(3000)
 
 server.addRoute('GET', '/', print)
+
 function print (request, response) {
   server.sendHTML(request, response, 'Consider it as HTML')
 }

11f2538f069cd589bcbcfe16b8272b883938f929
Manish <chandrajob365@gmail.com>
2017-07-24 14:53:43 +0530

    [test_server.js] update param of sendHtml to HTML form

--- test/test_server.js
+++ test/test_server.js
@@ -5,5 +5,12 @@ Server.start()
 Server.addRoute('GET', '/', print)
 
 function print (request, response) {
-  Server.sendHtml(request, response, 'Consider it as HTML')
+  Server.sendHtml(request, response, ` <HTML>
+    <head> </head>
+    <title> Test </title>
+    <body>
+    <h1> Hello </h1>
+    <input type = 'text' placeholder = 'Enter any thing' />
+    </body>
+    </HTML>`)
 }

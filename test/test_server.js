a070439168dfc419b67203abc4aad5f7afae0831
Manish <chandrajob365@gmail.com>
2017-07-25 23:05:34 +0530

    [test_server.js] Add function call on  POST

--- test/test_server.js
+++ test/test_server.js
@@ -2,15 +2,26 @@ const Server = require('../lib/server')
 
 Server.start()
 
-Server.addRoute('GET', '/', print)
+Server.addRoute('GET', '/about', displayForm)
+Server.addRoute('POST', '/about', showName)
 
-function print (request, response) {
+function displayForm (request, response) {
   Server.sendHtml(request, response, ` <HTML>
     <head> </head>
     <title> Test </title>
     <body>
     <h1> Hello </h1>
-    <input type = 'text' placeholder = 'Enter any thing' />
+    <form method='POST' action='/about'>
+      <input type = 'text' placeholder = 'Enter first Name' name = 'FName'/>
+
+      <button value = 'SendName'> Get Full Name </button>
+    </form>
     </body>
     </HTML>`)
+}
+
+function showName (request, response) {
+  console.log('<test_server.js showName > showName Entry')
+  console.log('<displayForm> @@@@@@@@@@@@@ req param =', request['params']['param1'])
+  Server.sendHtml(request, response, request.body)
 }

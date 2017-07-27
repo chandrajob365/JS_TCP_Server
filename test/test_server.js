be55ea5e94cd388bac69d07572d3791a1f0bea17
Manish <chandrajob365@gmail.com>
2017-07-28 02:09:33 +0530

    [test_server.js]Add showFileContent to display uploaded file contents

--- test/test_server.js
+++ test/test_server.js
@@ -1,9 +1,9 @@
 const Server = require('../lib/server')
-
+const fs = require('fs')
 Server.start()
 
-Server.addRoute('GET', '/about', displayForm)
-Server.addRoute('POST', '/about', showName)
+Server.addRoute('GET', '/', displayForm)
+Server.addRoute('POST', '/', showFileContent)
 
 function displayForm (request, response) {
   Server.sendHtml(request, response, ` <HTML>
@@ -11,19 +11,24 @@ function displayForm (request, response) {
     <title> Test </title>
     <body>
     <h2>Upload File</h2>
-    <form method="post" enctype="multipart/form-data" action="/about">
+    <form method="post" enctype="multipart/form-data" action="/">
       Who are you: <input type="text" name="username" /><br />
       What's your age: <input type="text" name="age" /><br />
-      <!-- Choose the file to upload:
-      <input type="file" name="fileID" /><br /> -->
+      Choose the file to upload:
+      <input type="file" name="fileID" /><br />
       <input type="submit" value="SEND" />
     </form>
     </body>
     </HTML>`)
 }
 //
-function showName (request, response) {
+function showFileContent (request, response) {
   console.log('<test_server.js showName > showName Entry')
-  console.log('<displayForm> request bodyparam =', request.body)
-  Server.sendHtml(request, response, JSON.stringify(request.body))
+  console.log('<showName> request bodyparam =', request.body)
+  console.log('<showName> request.files=', request.files)
+  fs.readFile(request.files.fileID.location, (err, data) => {
+    if (err) throw new Error(err)
+    console.log('<showName> Inside readFile data =', data)
+    Server.sendHtml(request, response, data + JSON.stringify(request.body))
+  })
 }

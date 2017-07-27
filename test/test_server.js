28b73373d10bad3099052bb8586bd34e4fe55aa9
Manish <chandrajob365@gmail.com>
2017-07-27 09:01:07 +0530

    [test_server.js]Add option to upload File and encoding as 'multipart/form-data\'

--- test/test_server.js
+++ test/test_server.js
@@ -10,18 +10,20 @@ function displayForm (request, response) {
     <head> </head>
     <title> Test </title>
     <body>
-    <h1> Hello </h1>
-    <form method='POST' action='/about'>
-      <input type = 'text' placeholder = 'Enter First Name' name = 'FName'/>
-      <input type = 'text' placeholder = 'Enter Last Name' name = 'LName'/>
-      <button value = 'SendName'> Get Full Name </button>
+    <h2>Upload File</h2>
+    <form method="post" enctype="multipart/form-data" action="/about">
+      Who are you: <input type="text" name="username" /><br />
+      What's your age: <input type="text" name="age" /><br />
+      <!-- Choose the file to upload:
+      <input type="file" name="fileID" /><br /> -->
+      <input type="submit" value="SEND" />
     </form>
     </body>
     </HTML>`)
 }
-
+//
 function showName (request, response) {
   console.log('<test_server.js showName > showName Entry')
   console.log('<displayForm> request bodyparam =', request.body)
-  Server.sendHtml(request, response, (request.body.FName + ' ' + request.body.LName))
+  Server.sendHtml(request, response, JSON.stringify(request.body))
 }

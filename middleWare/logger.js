95c60c70c4daafc6c1067d113bc886a8f3a2bdf9
Manish <chandrajob365@gmail.com>
2017-08-11 13:49:26 +0530

    [logger.js] Add logging based on mode and log level

--- middleWare/logger.js
+++ middleWare/logger.js
@@ -1,28 +1,33 @@
 const fs = require('fs')
 const mode = process.env.NODE_ENV || 'debug'
-let logStream = fs.createWriteStream('../logs/log.txt')
+let logStream = fs.createWriteStream('./logs/logs.txt')
 let log = ''
 
 const logger = (request, response, next) => {
   let ip = request.header['Host'].split(':')[0]
   let date = new Date().toISOString()
-  let method = request['METHOD']
-  let path = request['URI']
+  let method = request.header['METHOD']
+  let path = request.header['URI']
   log = ip + ' -- [' + date + '] ' + method + ' , ' + path
-  console.log('<logger> log = ', log)
+  next()
 }
 
 const logMsg = (level, msg) => {
-  // if (mode !== 'debug' && level !== 1) {
-    let message = getLevel(level) + ': ' + log + ' : ' + msg + ' \n'
-    logStream.write(message)
-  // }
+  if (mode === 'debug') { // All level logs should be logged in debug mode
+    createAndLogMsg(level, msg)
+  } else if (mode !== 'debug' && level !== 1 || level !== 2) { // In prod mode only error should be logged
+    createAndLogMsg(level, msg)
+  }
 }
 
+const createAndLogMsg = (level, msg) => {
+  let message = (getLevel(level) || 'INFO') + ': ' + log + ' : ' + (msg || '') + ' \n'
+  logStream.write(message)
+}
 const getLevel = level => {
-  if (level === 1) return 'info'
-  if (level === 2) return 'warning'
-  if (level === 3) return 'error'
+  if (level === 1) return 'INFO'
+  if (level === 2) return 'WARNING'
+  if (level === 3) return 'ERROR'
 }
 module.exports = {
   logger,

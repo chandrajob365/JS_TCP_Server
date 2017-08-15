360b68a41a74654b37baa0f6f2558c1a9e1dc5df
Manish <chandrajob365@gmail.com>
2017-08-16 02:02:51 +0530

    [test_server.js] Modify and add code to test static file handling and seprated HTML to separate file

--- test/test_server.js
+++ test/test_server.js
@@ -1,34 +1,66 @@
 const Server = require('../lib/server')
-const fs = require('fs')
+const Session = require('../middleWare/session')
+const logger = require('../middleWare/logger')
+const staticFileHandler = require('../middleWare/staticFileHandler')
 Server.start()
 
-Server.addRoute('GET', '/', displayForm)
-Server.addRoute('POST', '/', showFileContent)
+Server.use(Session.sessionHandler)
+Server.use(logger.logger)
+Server.use(staticFileHandler.handler)
 
-function displayForm (request, response) {
-  Server.sendHtml(request, response, ` <HTML>
-    <head> </head>
-    <title> Test </title>
-    <body>
-    <h2>Upload File</h2>
-    <form method="post" enctype="multipart/form-data" action="/">
-      Who are you: <input type="text" name="username" /><br />
-      What's your age: <input type="text" name="age" /><br />
-      Choose the file to upload:
-      <input type="file" name="fileID" /><br />
-      <input type="submit" value="SEND" />
-    </form>
-    </body>
-    </HTML>`)
+staticFileHandler.setViewLookUp('../public/views')
+
+Server.addRoute('GET', '/', login)
+Server.addRoute('POST', '/loginValidation', loginValidation)
+Server.addRoute('GET', '/home', home)
+Server.addRoute('POST', '/success', success)
+Server.addRoute('GET', '/logout', logout)
+Server.addAllowedOrigin('http://localhost:7000', ['GET', 'POST'])
+
+function login (request, response) {
+  let sessionData = Session.getSession(request)
+  logger.logMsg(1, sessionData)
+  if (sessionData && Object.keys(sessionData).length !== 0) {
+    Server.redirect(request, response, '/home')
+  } else {
+    staticFileHandler.renderView(request, response, 'index.html', () => {
+      Server.send(request, response)
+    })
+  }
 }
-//
-function showFileContent (request, response) {
-  console.log('<test_server.js showName > showName Entry')
-  console.log('<showName> request bodyparam =', request.body)
-  console.log('<showName> request.files=', request.files)
-  fs.readFile(request.files.fileID.location, (err, data) => {
-    if (err) throw new Error(err)
-    console.log('<showName> Inside readFile data =', data)
-    Server.sendHtml(request, response, data + JSON.stringify(request.body))
-  })
+
+function loginValidation (request, response) {
+  // Do some DB check for entered credentials
+  Session.addSession(request, {userName: request.body.userName})
+  Server.redirect(request, response, '/home')
+}
+
+function home (request, response) {
+  let sessionData = Session.getSession(request)
+  logger.logMsg(1, sessionData)
+  if (Object.keys(sessionData).length === 0) {
+    console.log('<home> Session cookie doesn\'t exist..............')
+    Server.redirect(request, response, '/')
+  } else {
+    staticFileHandler.renderView(request, response, 'form.html', () => {
+      Server.send(request, response)
+    })
+  }
+}
+
+function logout (request, response) {
+  Session.deleteSession(request)
+  Server.redirect(request, response, '/')
+}
+
+function success (request, response) {
+  let sessionData = Session.getSession(request)
+  console.log('<success> sessionData = ', sessionData)
+  if (Object.keys(sessionData).length === 0) {
+    Server.redirect(request, response, '/')
+  } else {
+    staticFileHandler.renderView(request, response, 'success.html', () => {
+      Server.send(request, response)
+    })
+  }
 }
